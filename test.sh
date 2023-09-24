e73f616a4ff4409e442915c7bedb1ab2679cfcc0
Elaina <userelaina@pm.me>
2023-09-24 12:45:16 +0000

    :white_check_mark: update `test.sh`

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 cd src
 python -m badapple --help
 python -m badapple --audio_player auto
-python -m badapple --audio badapple/badapple.wav
-python -m badapple --audio badapple/badapple.wav --audio_player avplay
+python -m badapple --audio_player mpv
+python -m badapple --audio_player aaaa
+python -m badapple --audio_player avplay
+python -m badapple --audio badapple/badapple.wav --audio_player vlc
+python -m badapple --audio badapple/badapple.mp3 --audio_player mpg123
+python -m badapple --audio badapple/badapple.mp3 --audio_player cmus
 python -m badapple --audio badapple/badapple.wav --audio_player simpleaudio
 python -m badapple --audio badapple/badapple.wav --audio_player pyaudio
 python -m badapple --audio badapple/badapple.wav --audio_player playsound
+python -m badapple --audio badapple/badapple.wav --audio_player pydub
 python -m badapple -i ../archive/87011701_p0.jpg -s 85:98
 python -m badapple -i ../archive/87011701_p0.jpg -s 85:98 --contrast
 python -m badapple -i ../archive/Elysia1.mp4 -o ../archive/ely.badapple -s 128:72 -r 10
-python -m badapple -i ../archive/ely.badapple --audio badapple/badapple.wav
+python -m badapple -i ../archive/ely.badapple --audio ../archive/addiction.flac
 python -m badapple -i ../archive/NCOP.mkv -s 96:54 --contrast --audio_player auto

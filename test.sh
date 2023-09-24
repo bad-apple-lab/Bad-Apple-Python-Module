82358598b6c6a38746bc25888bb8cd17f66901e3
Elaina <userelaina@pm.me>
2023-09-24 13:56:03 +0000

    :white_check_mark: update `test.sh`

--- test.sh
+++ test.sh
@@ -2,17 +2,17 @@ cd src
 python -m badapple --help
 python -m badapple --avaliable_player
 python -m badapple
-python -m badapple -i badapple/badapple.badapple --audio ../archive/your_everything.mp3
+python -m badapple -i _BADAPPLE_BADAPPLE --audio ../archive/your_everything.mp3
 python -m badapple --audio_player mpv
 python -m badapple --audio_player aaaa
 python -m badapple --audio_player avplay
-python -m badapple --audio badapple/badapple.wav --audio_player vlc
-python -m badapple --audio badapple/badapple.mp3 --audio_player mpg123
-python -m badapple --audio badapple/badapple.mp3 --audio_player cmus
-python -m badapple --audio badapple/badapple.wav --audio_player simpleaudio
-python -m badapple --audio badapple/badapple.wav --audio_player pyaudio
-python -m badapple --audio badapple/badapple.wav --audio_player playsound
-python -m badapple --audio badapple/badapple.wav --audio_player pydub
+python -m badapple --audio _BADAPPLE_WAV --audio_player vlc
+python -m badapple --audio _BADAPPLE_MP3 --audio_player mpg123
+python -m badapple --audio _BADAPPLE_MP3 --audio_player cmus
+python -m badapple --audio _BADAPPLE_WAV --audio_player simpleaudio
+python -m badapple --audio _BADAPPLE_MP3 --audio_player pyaudio
+python -m badapple --audio _BADAPPLE_WAV --audio_player playsound
+python -m badapple --audio _BADAPPLE_MP4 --audio_player pydub
 python -m badapple -i ../archive/87011701_p0.jpg -s 85:98
 python -m badapple -i ../archive/87011701_p0.jpg -s 85:98 --contrast
 python -m badapple -i ../archive/Elysia1.mp4 -o ../archive/ely.badapple -s 128:72 -r 10

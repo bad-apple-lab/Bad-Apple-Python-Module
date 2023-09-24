13d0a3e328689bdac548478ccc505a43dab74815
Elaina <userelaina@pm.me>
2023-09-24 16:03:29 +0000

    :white_check_mark: add `test1.sh`

---
+++ test1.sh
@@ -0,0 +1,16 @@
+py1 -m badapple --help
+py1 -m badapple --avaliable_player
+py1 -m badapple
+py1 -m badapple -i _BADAPPLE_BADAPPLE --audio archive/your_everything.mp3
+py1 -m badapple --audio_player mpv
+py1 -m badapple --audio_player aaaa
+py1 -m badapple --audio _BADAPPLE_WAV --audio_player vlc
+py1 -m badapple --audio _BADAPPLE_MP3 --audio_player mpg123
+py1 -m badapple --audio _BADAPPLE_MP3 --audio_player cmus
+py1 -m badapple --audio _BADAPPLE_WAV --audio_player simpleaudio
+py1 -m badapple --audio _BADAPPLE_MP4 --audio_player pydub
+py1 -m badapple -i archive/87011701_p0.jpg -s 85:98
+py1 -m badapple -i archive/87011701_p0.jpg -s 85:98 --contrast
+py1 -m badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+py1 -m badapple -i archive/ely.badapple --audio archive/addiction.flac
+py1 -m badapple -i archive/NCOP.mkv -s 96:54 --contrast --audio_player auto

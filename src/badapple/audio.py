80afe8d9314ec811f5dff7d120a70b07173c829e
Elaina <userelaina@pm.me>
2023-09-24 08:24:36 +0000

    :sparkles: more audio player

--- src/badapple/audio.py
+++ src/badapple/audio.py
@@ -1,62 +1,186 @@
 import os
-import threading
+import time
+import shlex
+import subprocess
+import multiprocessing
+from typing import Tuple, Callable
+from .util import which
 
-# __FFPLAY = 'ffplay'
-__PLAYSOUND = 'playsound'
+__FFPLAY = 'ffplay'  # all
+__AVPLAY = 'avplay'  # all
+# __PYDUB = 'pydub'  # simpleaudio-pyaudio-avplay-ffplay
+__SIMPLEAUDIO = 'simpleaudio'  # wav
+__PYAUDIO = 'pyaudio'  # wav
+__PLAYSOUND = 'playsound'  # mp3+wav not_win
+__AUTO = 'auto'  # for i in PLAYERS
 
-__NONE = 'none'
-# __ALL = 'all'
-__ALL_LIST = [__PLAYSOUND]
+PLAYERS = [
+    __FFPLAY,
+    __AVPLAY,
+    # __PYDUB,
+    __SIMPLEAUDIO,
+    __PYAUDIO,
+    __PLAYSOUND,
+]
 
-'''
-'qwq' is bad str
-'ffplay' unavailable
-'' get none
-'''
+
+def help_audio() -> None:
+    get_availables()
+    pass
 
 
-def available(player: str) -> bool:
-    if player not in __ALL_LIST:
-        return False
+def available(player: str) -> str:
+    if player == __AUTO:
+        return player
+
+    if player not in PLAYERS:
+        return ''
+
+    if player == __FFPLAY:
+        if which('ffplay'):
+            return player
+        else:
+            return ''
+
+    if player == __AVPLAY:
+        if which('avplay'):
+            return player
+        else:
+            return ''
+
+    if player == __SIMPLEAUDIO:
+        try:
+            import simpleaudio
+        except ImportError:
+            return ''
+        return player
+
+    if player == __PYAUDIO:
+        try:
+            import pyaudio
+        except ImportError:
+            return ''
+        return player
+
     if player == __PLAYSOUND:
         try:
             import playsound
         except ImportError:
-            return False
-        return True
+            return ''
+        return player
 
 
-def get_available() -> str:
-    for player in __ALL_LIST:
-        if available(player):
-            return player
-    return __NONE
+def get_availables() -> list:
+    return [i for i in PLAYERS if available(i)]
 
 
-def preplaya(audio: str, player: str, video: str = None) -> tuple:
-    if audio:
-        audio = os.path.abspath(audio)
-        if not player:
-            player = get_available()
-            if player == __NONE:
-                raise ValueError('No available audio player')
-    else:
+def preplaya(
+    audio: str,
+    player: str,
+    video: str = None,
+    check_player: bool = True
+) -> Tuple[str, str]:
+    if not audio:
         if not player:
             return '', ''
         if video is None:
             raise FileNotFoundError(audio)
         audio = video
-        if player not in __ALL_LIST:
-            raise ValueError(player)
-        if not available(player):
-            raise ImportError(player)
+
+    audio = os.path.abspath(audio)
     open(audio, 'rb').close()
+
+    if player == __AUTO or not player:
+        return audio, __AUTO
+
+    if player not in PLAYERS:
+        raise ValueError('Player %s is not in players' % player)
+    if check_player:
+        if not available(player):
+            raise ValueError('Player %s is not available' % player)
+
     return audio, player
 
 
-def playa(audio: str):
+def f_ffplay(audio: str) -> None:
+    subprocess.call(
+        shlex.split('ffplay -nodisp -autoexit -hide_banner "%s"' % audio),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+
+def f_avplay(audio: str) -> None:
+    subprocess.call(
+        shlex.split('avplay -nodisp -autoexit -hide_banner "%s"' % audio),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+
+def f_simpleaudio(audio: str) -> None:
+    import simpleaudio
+    wave_obj = simpleaudio.WaveObject.from_wave_file(audio)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    play_obj.stop()
+
+
+def f_pyaudio(audio: str) -> None:
+    import wave
+    import pyaudio
+    CHUNK = 1024
+    with wave.open(audio, 'rb') as wf:
+        p = pyaudio.PyAudio()
+        stream = p.open(
+            format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True
+        )
+        data = wf.readframes(CHUNK)
+        while len(data):
+            stream.write(data)
+            data = wf.readframes(CHUNK)
+        stream.close()
+        p.terminate()
+
+
+def f_playsound(audio: str) -> None:
     from playsound import playsound
-    audio = audio.replace('\\', '/')
-    _t = threading.Thread(target=playsound, args=(audio,), daemon=True)
-    _t.start()
-    return _t
+    playsound(audio)
+
+
+def f_auto(audio: str) -> None:
+    for i in PLAYERS:
+        a = time.time()
+        try:
+            f_playa(audio, i)
+            return
+        except KeyboardInterrupt:
+            return
+        except Exception:
+            if time.time()-a > 1.:
+                raise RuntimeError(i)
+
+
+def f_playa(audio: str, player: str) -> None:
+    if player == __AUTO:
+        return f_auto(audio)
+    if player == __SIMPLEAUDIO:
+        return f_simpleaudio(audio)
+    if player == __PYAUDIO:
+        return f_pyaudio(audio)
+    if player == __FFPLAY:
+        return f_ffplay(audio)
+    if player == __AVPLAY:
+        return f_avplay(audio)
+    if player == __PLAYSOUND:
+        return f_playsound(audio)
+    raise ValueError(player)
+
+
+def p_playa(audio: str, player: str) -> None:
+    p = multiprocessing.Process(target=f_playa, args=(audio, player))
+    p.start()
+    return p
--- src/badapple/util.py
+++ src/badapple/util.py
@@ -1,5 +1,6 @@
 import os
 import time
+from typing import Tuple, Callable
 
 
 def console_size(x: int, y: int) -> int:
@@ -13,6 +14,17 @@ def rewind() -> None:
 
 def clear() -> None:
     print('\x1b[0J', end='', flush=True)
+
+
+def func_pass() -> None:
+    pass
+
+
+def get_func(need_clear: bool = True) -> Tuple[Callable, Callable, Callable]:
+    if need_clear:
+        return rewind, clear, console_size
+    else:
+        return print, func_pass, func_pass
 
 
 class Font:
@@ -41,3 +53,15 @@ class Timer:
 
     def slp(self, s: int = 1) -> None:
         time.sleep(s)
+
+
+def which(program: str) -> str:
+    if os.name == "nt" and not program.lower().endswith(".exe"):
+        program += ".EXE"
+
+    envdir_list = [os.curdir] + os.environ["PATH"].split(os.pathsep)
+
+    for envdir in envdir_list:
+        program_path = os.path.join(envdir, program)
+        if os.path.isfile(program_path) and os.access(program_path, os.X_OK):
+            return program_path

ae55deac564e94489240bc12be4f0b0a02e88c37
Elaina <userelaina@pm.me>
2023-09-28 00:22:07 +0000

    :white_check_mark: update `test.sh`

--- test.sh
+++ test.sh
@@ -1,20 +1,18 @@
-cd src
-py -m badapple --help
-py -m badapple --help_audio
-py -m badapple
-py -m badapple -i aaaa
-py -m badapple -i _BA_BA --audio ../archive/your_everything.mp3
-py -m badapple --audio_player mpv
-py -m badapple --audio_player aaaa
-py -m badapple --audio_player avplay
-py -m badapple --audio _BA_WAV --audio_player vlc
-py -m badapple --audio _BA_MP3 --audio_player mpg123
-py -m badapple --audio _BA_MP3 --audio_player cmus
-py -m badapple --audio _BA_WAV --audio_player simpleaudio
-py -m badapple --audio _BA_WAV --audio_player playsound
-py -m badapple --audio _BA_MP4 --audio_player pydub
-py -m badapple -i ../archive/87011701_p0.jpg -s 85:98
-py -m badapple -i ../archive/87011701_p0.jpg -s 85:98 --contrast
-py -m badapple -i ../archive/Elysia1.mp4 -o ../archive/ely.badapple -s 128:72 -r 10
-py -m badapple -i ../archive/ely.badapple --audio ../archive/addiction.flac
-py -m badapple -i ../archive/NCOP.mkv -s 96:54 --contrast --audio_player auto
+py1 -m src.badapple --help
+py1 -m src.badapple --help_audio
+py1 -m src.badapple
+py1 -m src.badapple -i aaaa
+py1 -m src.badapple -i _BA_BA --audio archive/your_everything.mp3
+py1 -m src.badapple --audio_player mpv
+py1 -m src.badapple --audio_player aaaa
+py1 -m src.badapple --audio _BA_WAV --audio_player vlc
+py1 -m src.badapple --audio _BA_MP3 --audio_player mpg123
+py1 -m src.badapple --audio _BA_MP3 --audio_player cmus
+py1 -m src.badapple --audio _BA_WAV --audio_player simpleaudio
+py1 -m src.badapple --audio _BA_WAV --audio_player playsound
+py1 -m src.badapple --audio _BA_MP4 --audio_player pydub
+py1 -m src.badapple -i archive/87011701_p0.jpg -s 85:98
+py1 -m src.badapple -i archive/87011701_p0.jpg -s 85:98 --contrast
+py1 -m src.badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+py1 -m src.badapple -i archive/ely.badapple --audio archive/addiction.flac
+py1 -m src.badapple -i archive/NCOP.mkv -s 96:54 --contrast --audio_player auto

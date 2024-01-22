05a80bab624341ec0a5cc042c9a7f939a1e176b6
Elaina <userelaina@pm.me>
2024-01-22 00:49:31 +0000

    :white_check_mark: test for auto scale

--- test.sh
+++ test.sh
@@ -21,11 +21,10 @@ py1 -m src.badapple --audio _BA_MP3 --audio_player mpg123
 py1 -m src.badapple --audio _BA_MP3 --audio_player cmus
 py1 -m src.badapple --audio _BA_WAV --audio_player simpleaudio
 py1 -m src.badapple --audio _BA_WAV --audio_player playsound
-py1 -m src.badapple -i archive/87011701_p0.jpg -s 85:98
 py1 -m src.badapple -i archive/87011701_p0.jpg -s 85:98 --contrast
-py1 -m src.badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+py1 -m src.badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 0:72 -r 10
 py1 -m src.badapple -i archive/ely.badapple --audio archive/addiction.flac
-py1 -m src.badapple -i archive/NCOP.mkv -s 96:54 --contrast --audio_player auto
+py1 -m src.badapple -i archive/NCOP.mkv -s 96:0 --contrast --audio_player auto
 
 py -m build
 twine check dist/*
@@ -42,9 +41,8 @@ py1 -m badapple --audio _BA_MP3 --audio_player mpg123
 py1 -m badapple --audio _BA_MP3 --audio_player cmus
 py1 -m badapple --audio _BA_WAV --audio_player simpleaudio
 py1 -m badapple --audio _BA_WAV --audio_player playsound
-py1 -m badapple -i archive/87011701_p0.jpg -s 85:98
 py1 -m badapple -i archive/87011701_p0.jpg -s 85:98 --contrast
-py1 -m badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 128:72 -r 10
+py1 -m badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 0:72 -r 10
 py1 -m badapple -i archive/ely.badapple --audio archive/addiction.flac
-py1 -m badapple -i archive/NCOP.mkv -s 96:54 --contrast --audio_player auto
+py1 -m badapple -i archive/NCOP.mkv -s 96:0 --contrast --audio_player auto
 twine upload dist/*

72af8b3f546495cd354ccab27e142feda37f7cf8
userElaina <userelaina@pm.me>
2024-04-15 14:30:01 +0000

    :sparkles: x256

--- src/badapple/color.py
+++ src/badapple/color.py
@@ -5,11 +5,19 @@ from .util import ansi_available
 
 COLOR_ASCII = 'ascii'
 COLOR_RGB24 = 'rgb24'
+COLOR_X256E = 'x256e'
+COLOR_X256W = 'x256w'
 
 COLOR_TYPE = [COLOR_ASCII]
 
 if ansi_available():
     COLOR_TYPE.append(COLOR_RGB24)
+    try:
+        import x256offline
+        COLOR_TYPE.append(COLOR_X256E)
+        COLOR_TYPE.append(COLOR_X256W)
+    except ImportError:
+        pass
 
 
 def get_buffer(
@@ -53,6 +61,38 @@ def get_buffer(
         for j in range(y//2):
             for k in range(x):
                 buffer += '\x1b[48;2;%d;%d;%dm ' % tuple(img[j, k][-1::-1])
+            buffer += '\x1b[0m\n'
+
+    elif color == COLOR_X256E:
+        import x256offline as x256
+        img = cv2.resize(img, (x, y//2))
+        # np.ndarray(shape=(y, x, 3), dtype=np.uint8)
+        # x*y subpixel == x*y pixel == x*y char
+        for j in range(y//2):
+            for k in range(x):
+                tuple(img[j, k][-1::-1])
+                buffer += '\x1b[48;5;%dm ' % x256.from_rgb(
+                    img[j, k, 2],
+                    img[j, k, 1],
+                    img[j, k, 0],
+                    False
+                )
+            buffer += '\x1b[0m\n'
+
+    elif color == COLOR_X256W:
+        import x256offline as x256
+        img = cv2.resize(img, (x, y//2))
+        # np.ndarray(shape=(y, x, 3), dtype=np.uint8)
+        # x*y subpixel == x*y pixel == x*y char
+        for j in range(y//2):
+            for k in range(x):
+                tuple(img[j, k][-1::-1])
+                buffer += '\x1b[48;5;%dm ' % x256.from_rgb(
+                    img[j, k, 2],
+                    img[j, k, 1],
+                    img[j, k, 0],
+                    True
+                )
             buffer += '\x1b[0m\n'
 
     return buffer

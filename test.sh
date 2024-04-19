88d40bc6853b9d6abf1f4f42bbc9730c39177672
userElaina <userelaina@pm.me>
2024-04-19 15:25:49 +0000

    :white_check_mark: 爱莉希雅死了

--- test.sh
+++ test.sh
@@ -23,6 +23,8 @@ py1 -m src.badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 160:0 -c r
 py1 -m src.badapple -i archive/ely.badapple --audio archive/addiction.flac
 py1 -m src.badapple -i archive/NCOP.mkv -s 192:108 -r 10 -c x232w --audio_player auto
 py1 -m src.badapple -i archive/1.mp4 -s 192:108 -r 10 -c rgb24 --audio_player auto
+py1 -m src.badapple -i archive/Elysia6.mp4 -s 240:0 -r 5 -c fullwidth -m "爱莉希雅死了" --audio_player auto
+py1 -m src.badapple -i archive/Elysia6.mp4 -s 240:0 -r 5 -c halfwidth -m "Elysia" --audio_player auto
 
 py -m build
 twine check dist/*
@@ -45,4 +47,6 @@ py1 -m badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 160:0 -c rgb24
 py1 -m badapple -i archive/ely.badapple --audio archive/addiction.flac
 py1 -m badapple -i archive/NCOP.mkv -s 192:108 -r 10 -c x232w --audio_player auto
 py1 -m badapple -i archive/1.mp4 -s 192:108 -r 10 -c rgb24 --audio_player auto
+py1 -m badapple -i archive/Elysia6.mp4 -s 240:0 -r 5 -c fullwidth -m "爱莉希雅死了" --audio_player auto
+py1 -m badapple -i archive/Elysia6.mp4 -s 240:0 -r 5 -c halfwidth -m "Elysia" --audio_player auto
 twine upload dist/*

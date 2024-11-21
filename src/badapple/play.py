6e37593e863ca42a56d2dd8c3c4602d79125b37d
userElaina <userelaina@pm.me>
2024-11-21 13:59:51 +0000

    :art: change variable name

--- src/badapple/play.py
+++ src/badapple/play.py
@@ -29,22 +29,22 @@ def play(
     p = get_player(audio, player, video)
 
     capture = cv2.VideoCapture(video)
-    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-    width = int(width + 0.5)
-    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    height = int(height + 0.5)
+    ori_x = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    ori_x = int(ori_x + 0.5)
+    ori_y = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    ori_y = int(ori_y + 0.5)
     nb_frames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     nb_frames = int(nb_frames + 0.5)
-    rate = capture.get(cv2.CAP_PROP_FPS)
-    if rate < 0.1:
-        raise Exception("The frame rate is %s!" % str(rate))
-    duration = nb_frames / rate
+    ori_fps = capture.get(cv2.CAP_PROP_FPS)
+    if ori_fps < 0.1:
+        raise Exception("The frame rate is %s!" % str(ori_fps))
+    duration = nb_frames / ori_fps
 
-    mo = max(int(0.5 + rate / fps), 1)
-    clk = mo / rate
+    n_frames_get_1 = max(int(0.5 + ori_fps / fps), 1)
+    clk = n_frames_get_1 / ori_fps
 
-    max_x, max_y = os.get_terminal_size()
-    max_y = (max_y-1)*2
+    terminal_x, terminal_y = os.get_terminal_size()  # columns, lines
+    terminal_y = (terminal_y-1)*2
 
     x = int(x)
     y = int(y)
@@ -56,25 +56,25 @@ def play(
             pass
         else:
             # x = x
-            y = int(height*x/width + 0.5)
+            y = int(ori_y*x/ori_x + 0.5)
     else:
         if y > 0:
-            x = int(width*y/height + 0.5)
+            x = int(ori_x*y/ori_y + 0.5)
             # y = y
         else:
-            x = min(max_x, int(width*max_y/height + 0.5))
-            y = min(max_y, int(height*max_x/width + 0.5))
+            x = min(terminal_x, int(ori_x*terminal_y/ori_y + 0.5))
+            y = min(terminal_y, int(ori_y*terminal_x/ori_x + 0.5))
 
     if y % 2:
-        if y == max_y+1:
-            y = max_y
+        if y == terminal_y+1:
+            y = terminal_y
         else:
             y += 1
 
     print('[%d:%d %.2lfHz] -%s-> [%d:%d %.2lfHz] %.3lfs/%dms%s %s' % (
-        width, height, rate,
+        ori_x, ori_y, ori_fps,
         p.name if p else '',
-        x, y, rate / mo,
+        x, y, ori_fps / n_frames_get_1,
         duration, clk*1000+0.5,
         ' [debug]' if debug else '',
         message
@@ -99,7 +99,7 @@ def play(
                 succ, img = capture.read()
                 if not succ:
                     raise Exception(i)
-                if i % mo:
+                if i % n_frames_get_1:
                     continue
                 buffer = get_buffer(
                     img, x, y, color, message,
@@ -131,7 +131,7 @@ def play(
             succ, img = capture.read()
             if not succ:
                 raise Exception(i)
-            if i % mo:
+            if i % n_frames_get_1:
                 continue
             buffer = get_buffer(
                 img, x, y, color, message,

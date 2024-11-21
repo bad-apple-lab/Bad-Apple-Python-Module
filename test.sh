0ae745d0105d8e8d941d515709fb4d83df12ab1d
userElaina <userelaina@pm.me>
2024-11-21 16:32:40 +0000

    :white_check_mark: update `test.sh`

--- test.sh
+++ test.sh
@@ -19,12 +19,12 @@ py1 -m src.badapple --audio _BA_WAV --audio_player simpleaudio
 py1 -m src.badapple --audio _BA_WAV --audio_player pyaudio
 py1 -m src.badapple --audio _BA_WAV --audio_player playsound
 py1 -m src.badapple -i archive/87011701_p0.jpg -s 85:98 -c ascii --contrast
-py1 -m src.badapple -i archive/Elysia1.mp4 -o archive/ely.badapple -s 160:0 -c rgb24
+py1 -m src.badapple -i archive/Elysia7.mp4 -o archive/ely.badapple -s 160:0 -c rgb24
 py1 -m src.badapple -i archive/ely.badapple --audio archive/addiction.flac
 py1 -m src.badapple -i archive/NCOP.mkv -s 120:0 -r 10 -c x232w --audio_player auto
 py1 -m src.badapple -i archive/NCOP.mkv -s 192:108 -r 10 -c rgb24 --audio_player auto
 py1 -m src.badapple -i archive/1.mp4 -s 192:108 -r 10 -c rgb24 --audio_player auto
-py -m src.badapple -i archive/e.mp4 -r 10 -c fullwidth -m "爱莉希雅死了" --audio_player auto
+py1 -m src.badapple -i archive/Elysia1.mp4 -r 10 -c fullwidth -m "爱莉希雅死了" --audio_player auto
 py1 -m src.badapple -i archive/Elysia6.mp4 -s 240:0 -r 5 -c halfwidth -m "Elysia" --audio_player auto
 
 py -m build
